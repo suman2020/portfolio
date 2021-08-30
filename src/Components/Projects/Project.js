3be36a5d9b3b2df5f3615aabcb0925efb69b2a60
suman2020 <magarsuman2020@gmail.com>
2021-08-30 18:36:32 -0500

    added a hero section

--- src/Components/Projects/Project.js
+++ src/Components/Projects/Project.js
@@ -1,13 +1,43 @@
-import { ProjectsContainer } from "./ProjectElements"
+import { ProjectsContainer,ProjectHolder,Content,Title,Description,Demo, LiveButton,ViewSourceCode } from "./ProjectElements"
 
 const Projects = () =>{
     return(
-        <>
         <div id="projects">
-        <h1>This is project folder.</h1>
-        <ProjectsContainer/>
+            <h1 style={{textAlign:'center'}}>Projects</h1>
+            <ProjectsContainer >
+            
+                <ProjectHolder>
+                    
+                    <Content>
+                        <Title>
+                        FaceRecognition
+                        </Title>
+                        
+                        <Description>
+
+                        </Description>
+                    </Content>
+                    <Demo>
+                        <LiveButton>
+                            ViewLive
+                        </LiveButton>
+                        <ViewSourceCode>
+                            SourceCode
+
+                        </ViewSourceCode>
+                    </Demo>
+
+                </ProjectHolder>
+                <ProjectHolder>
+                    Hi
+                </ProjectHolder>
+                <ProjectHolder>
+                    Hey
+                </ProjectHolder>
+            </ProjectsContainer>
         </div>
-        </>
+        
+        
     );
 }
 export default Projects;
--- src/Components/Projects/ProjectElements.js
+++ src/Components/Projects/ProjectElements.js
@@ -2,12 +2,59 @@ import styled from 'styled-components';
 export const ProjectsContainer = styled.div`
     
     display: grid;
-    grid-template-columns: 1fr 1fr;
+    grid-template-columns: 1fr;
     
     // align-items:center;
-    padding: 0 30px;
-    height: 50vh;
+    padding: 5px 30px;
+    height: 90vh;
     position: relative;
     z-index: 1;
     background: blue;
-    `
+    row-gap: 1em;
+`
+export const ProjectHolder = styled.div`
+    display: flex;
+    justify-content:space-between;  
+    border: 1px black solid;
+    
+
+`
+export const Title = styled.h2`
+    font-size: 1.5em;
+    text-align: center;
+    color: palevioletred;
+`
+export const Content = styled.section`
+    height:100%;
+    width: 40%;
+    background-color: red;
+
+`
+export const Description = styled.p`
+    color: yellow;
+`
+export const Demo = styled.section`
+    display: flex;
+    justify-content: space-around;
+    align-items: flex-end;
+    height: 100%;
+    width: 60%;
+    background-color: grey;
+`
+
+export const LiveButton = styled.button`
+    border-radius: 10%;
+    font-size: 20px;
+    height: 30%;
+    padding: 15px 32px;
+    text-align: center;
+
+`
+export const ViewSourceCode = styled.button`
+    border-radius: 10%;
+    font-size: 20px;
+    padding: 15px 32px;
+    height: 30%;
+
+`
+
